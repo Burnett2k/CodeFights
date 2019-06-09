761b3fe5edb31864da122ac98ecad6622390b601
Sawyer Burnett <sawyerburnett@gmail.com>
2019-06-09 13:54:14 -0700

    add problem text

--- arcade/15/solution.js
+++ arcade/15/solution.js
@@ -1,27 +1,35 @@
+// Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+// Example
+// For
+// picture = ["abc",
+//            "ded"]
+// the output should be
+// addBorder(picture) = ["*****",
+//                       "*abc*",
+//                       "*ded*",
+//                       "*****"]
+
 function addBorder(picture) {
-    
-    
-    
-    if (!picture || picture.length === 0) {
-        return [];
-    }
-    //get length of first item
-    let l = picture[0].length;
-    //create a new array
-    let result = [];
-    
-    //add row to top with l + 2 characters
-    let horizontalBorder = "*".repeat(l + 2);
-    result.push(horizontalBorder);
-    
-    //loop through each item, add * to beginning and end of string and then add to array
-    for (let i = 0; i < picture.length; i++) {
-        result.push(`*${picture[i]}*`);
-    }
-    
-    //add row to bottom with l + 2 characters
-    result.push(horizontalBorder);
-    //return
-    
-    return result;
+  if (!picture || picture.length === 0) {
+    return [];
+  }
+  //get length of first item
+  let l = picture[0].length;
+  //create a new array
+  let result = [];
+
+  //add row to top with l + 2 characters
+  let horizontalBorder = "*".repeat(l + 2);
+  result.push(horizontalBorder);
+
+  //loop through each item, add * to beginning and end of string and then add to array
+  for (let i = 0; i < picture.length; i++) {
+    result.push(`*${picture[i]}*`);
+  }
+
+  //add row to bottom with l + 2 characters
+  result.push(horizontalBorder);
+  //return
+
+  return result;
 }
