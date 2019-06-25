c221536e416f42f2bd168513a7219a11919b195c
Sawyer Burnett <sawyerburnett@gmail.com>
2019-06-24 19:39:12 -0700

    adding initial problem

---
+++ arcade/17/solution.js
@@ -0,0 +1,8 @@
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+// Example
+
+// For inputArray = [1, 1, 1], the output should be
+// make next char greater -> [1,2,1] = counter = 1
+// make next char greater -> [1,2,3] = counter = 3
+// arrayChange(inputArray) = 3.
