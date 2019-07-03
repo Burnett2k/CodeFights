6e0ecc7994035b45912d461781d5a32360886c2c
Sawyer Burnett <sawyerburnett@gmail.com>
2019-07-03 15:43:00 -0700

    update template to have error handling

--- create-template.sh
+++ create-template.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# create directory
-mkdir arcade/$1
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied, please pass question number as input"
+    exit 1
+else
+    # create directory
+    mkdir arcade/$1
 
-# copy template file and rename
-cp template.js arcade/$1/solution.js
+    # copy template file and rename
+    cp template.js arcade/$1/solution.js
+
+    echo created directory @ arcade/$1/solution.js
+fi
+
